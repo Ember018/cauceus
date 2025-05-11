01738b18c5dccaac6cdf53325e06fd976b9a3fe2
Flicker028 <flicker028@proton.me>
2025-05-11 22:28:02 +0700

    Implements user apis

--- cauceus_backend/users/serializers.py
+++ cauceus_backend/users/serializers.py
@@ -1,1 +1,42 @@
+from rest_framework import serializers
+from django.contrib.auth import authenticate
+from .models import User
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'country', 'city', 'postal_code', 'phone_number', 'preferred_contact_method')
+        read_only_fields = ('id',)
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username = validated_data['username'],
+            email = validated_data['email'],
+            password = validated_data['password']
+        )
+        return user
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, attrs):
+        username = attrs.get("username")
+        password = attrs.get("password")
+
+        if not (username and password):
+            raise serializers.ValidationError("Please provide both username and password")
+        user = authenticate(request = self.context.get('request'), username=username, password=password)
+        if not user:
+            raise serializers.ValidationError("Invalid credentials")
+        if not user.is_active:
+            raise serializers.ValidationError("User not active")
+
+        attrs['user'] = user
+        return attrs
--- cauceus_backend/users/views.py
+++ cauceus_backend/users/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+from rest_framework import generics, views, status
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from .models import User
+from .serializers import  LoginSerializer, UserSerializer, UserRegistrationSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 
-# Create your views here.
+class UserListView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetailView(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class =UserSerializer
+
+class UserRegistrationView(generics.CreateAPIView):
+    serializer_class = UserRegistrationSerializer
+
+class LoginView(views.APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = LoginSerializer(data=request.data, context={'request':request})
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key, 'user_id': user.pk, 'email': user.email}, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LogoutView(views.APIView):
+    authentication_classes = [TokenAuthentication]
+    premission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        try:
+            request.user.auth_token.delete()
+            return Response({"detail": "Sucessfully logged out!"})
+        except AttributeError:
+            return Response({"detail": "No active token found for this user"})
+        except Exception as e:
+            return Response({"detail": f"Could not log out, or no active session. {e}"})
